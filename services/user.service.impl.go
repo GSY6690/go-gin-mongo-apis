858e4e61963ce56da05b6738f6cdce361e78912e
GSY6690 <71131092+GSY6690@users.noreply.github.com>
2022-07-14 21:34:56 +0900

    Update user.service.impl.go

--- services/user.service.impl.go
+++ services/user.service.impl.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 
-	"github.com/DevProblems/sarang-gin-mongo-apis/models"
+	"github.com/GSY6690/go-gin-mongo-apis/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
